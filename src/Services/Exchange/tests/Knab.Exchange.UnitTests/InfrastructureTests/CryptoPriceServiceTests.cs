c38f7224b39397260fac7150e49cbf37281e7590
Mohammad Olfatmiri <m.olfatmiri@freetimecompany.nl>
2022-05-21 18:10:46 +0200

    Deleted unnecessary files.

--- src/Services/Exchange/tests/Knab.Exchange.UnitTests/InfrastructureTests/CryptoPriceServiceTests.cs
+++ src/Services/Exchange/tests/Knab.Exchange.UnitTests/InfrastructureTests/CryptoPriceServiceTests.cs
@@ -3,7 +3,7 @@ using Knab.Exchange.Infrastructure.Repositories.CryptoPriceRepository;
 using Knab.Exchange.Infrastructure.Services;
 using Knab.Exchange.UnitTests.Mocks;
 
-namespace Knab.Exchange.UnitTests.ApplicationServiceTests
+namespace Knab.Exchange.UnitTests.InfrastructureTests
 {
     public class CryptoPriceServiceTests
     {
@@ -12,12 +12,12 @@ namespace Knab.Exchange.UnitTests.ApplicationServiceTests
         {
             var symbol = "CRYPTO";
             var cryptoPrice = 100.0;
-            var exchangeRates = new ExchangeRates(1,1.1,1.2,1.3,1.4);
+            var exchangeRates = new ExchangeRates(1, 1.1, 1.2, 1.3, 1.4);
             var resultPriceList = new CryptoPriceList(
-                symbol, 
+                symbol,
                 cryptoPrice * exchangeRates.Usd, cryptoPrice * exchangeRates.Eur,
                 cryptoPrice * exchangeRates.Brl, cryptoPrice * exchangeRates.Gbp, cryptoPrice * exchangeRates.Aud);
-            
+
             var mockCryptoPriceRepo = MockICryptoPriceRepository.ValidCryptoPriceRepository(cryptoPrice);
             var mockExchangeRateRepo = MockIExchangeRateRepository.ValidExchangeRatesRepository(exchangeRates);
 
